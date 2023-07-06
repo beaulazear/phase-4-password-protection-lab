7f07b4dc7902d1c5d5dbab5da7820971d5d43863
Beau <beaulazear@gmail.com>
2023-07-05 20:35:28 -0400

    commit tree 63e222a3cc2497f3db4b3f772df925c0d202d8c8

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # resources :users
+  get '/me', to: 'users#show'
+  post '/signup', to: 'users#create'
+  delete '/logout', to: 'sessions#destroy'
+  post '/login', to: 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
